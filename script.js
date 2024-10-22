43bb3041bd74188bbedf71d7e4ea92435697cbd2
Desterm2 <josue.amaral.maciel25@gmail.com>
2024-10-21 21:33:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Função para futuras interações ou funcionalidades que você queira adicionar
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website da House City carregado com sucesso!");
-});
