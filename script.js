77c6ef9de28c3df50811095981af173a95295149
Desterm2 <josue.amaral.maciel25@gmail.com>
2024-10-21 21:35:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função para futuras interações ou funcionalidades que você queira adicionar
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website da House City carregado com sucesso!");
+});
